a5c9022fcc3d368e29b177bc602f677ef96f8cc2
Junior Francis <juniorfrancis91@gmail.com>
2023-05-11 18:39:38 -0300

    fluxo contabilidade

--- src/main/java/com/maney/api/service/AccountingService.java
+++ src/main/java/com/maney/api/service/AccountingService.java
@@ -1,4 +1,11 @@
 package com.maney.api.service;
 
+import com.maney.api.model.responses.AccountingResponse;
+
+import java.time.LocalDate;
+
 public interface AccountingService {
+
+    AccountingResponse overview(LocalDate dateToQuery);
+
 }
--- src/main/java/com/maney/api/utils/DateUtils.java
+++ src/main/java/com/maney/api/utils/DateUtils.java
@@ -2,20 +2,29 @@ package com.maney.api.utils;
 
 import com.maney.api.constants.Brand;
 import com.maney.api.model.Card;
+import com.maney.api.repository.CardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
+@Component
 public class DateUtils {
 
-    public static List<LocalDate> parsePeriod(LocalDate rawDate, Card card, Integer daysToCloseCard){
+    @Autowired
+    CardRepository cardRepository;
+
+    @Value("${card.days_to_close_card}")
+    private final Integer DAYS_TO_CLOSE_CARD = 7;
+
+    public List<LocalDate> parsePeriod(LocalDate rawDate, Card card){
         //TODO Montar Strateg Pattern to chose the logic to less days
         //TODO Transformar isso em annotation para que possamos passar o daysToCloseCard diretamente aqui.
         Integer dueDay = card.getDueDay();
 
-        LocalDate initialDate = rawDate.withDayOfMonth(dueDay).minusDays(daysToCloseCard);
+        LocalDate initialDate = rawDate.withDayOfMonth(dueDay).minusDays(DAYS_TO_CLOSE_CARD);
         LocalDate finalDate;
 
         if(card.getBrand() != null && card.getBrand().equals(Brand.VISA)) {
@@ -31,6 +40,24 @@ public class DateUtils {
         Collections.sort(period);
 
         return period;
+    }
+
+    public Map<Card, List<LocalDate>> parsePeriodToAllCards(LocalDate rawDate){
+
+        List<Card> cards = cardRepository.findAll();
+
+        Map<Card, List<LocalDate>> periodsByCard = new HashMap<>(Map.of());
+
+        cards.forEach( card -> {
+
+            List<LocalDate> period = this.parsePeriod(rawDate, card);
+
+            periodsByCard.put(card, period);
+
+        });
+
+        return periodsByCard;
+
     }
 
 }
